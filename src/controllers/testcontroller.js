cd31db2a2dc46de3c22d2e7666696ee7238c5931
Arfrial <156853495+Arfrial@users.noreply.github.com>
2026-03-10 13:14:41 -0400

    project files initialization

---
+++ src/controllers/testcontroller.js
@@ -0,0 +1,9 @@
+// Placeholder controller functions for Test Mode
+
+exports.placeShips = (req, res) => {};
+
+exports.revealBoard = (req, res) => {};
+
+exports.resetGame = (req, res) => {};
+
+exports.setTurn = (req, res) => {};
---
+++ src/routes/testroutes.js
@@ -0,0 +1,6 @@
+// Test Mode Routes for Automated Grading
+
+// POST /test/games/{gameId}/ships
+// GET /test/games/{gameId}/board
+// POST /test/games/{gameId}/reset
+// POST /test/games/{gameId}/set-turn
