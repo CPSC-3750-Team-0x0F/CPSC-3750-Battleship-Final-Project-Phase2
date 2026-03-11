16912177738cbabbe4c94ede9cf1de81063cdaed
Christian Johnston <christrontherobot@gmail.com>
2026-03-11 14:32:59 -0400

    updated paths in gameRoutes

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const gameController = require("../controllers/gameController");
 
+// Matches POST /api/games
 router.post("/", gameController.createGame);
+
+// Matches POST /api/games/{id}/join
 router.post("/:id/join", gameController.joinGame);
+
+// Matches GET /api/games/{id}
 router.get("/:id", gameController.getGame);
 
 module.exports = router;
