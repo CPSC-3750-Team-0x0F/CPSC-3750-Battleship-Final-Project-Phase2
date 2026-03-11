478048081af7761d45b098c2f08cef4ce4fdd770
Arfrial <arfrial@clemson.edu>
2026-03-10 21:22:30 -0400

    testController rename fix

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const testController = require('./testController');
+const testController = require("../controllers/testController");
 
 // Security Middleware: Checks for X-Test-Mode header
 const verifyTestMode = (req, res, next) => {
